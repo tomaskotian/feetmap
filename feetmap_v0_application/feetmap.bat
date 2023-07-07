321eb029d7fdf9018bd77187a911ed4230721df6
tomaskotian <98422581+tomaskotian@users.noreply.github.com>
2023-07-07 16:35:58 +0200

    Delete feetmap.bat

--- feetmap_v0_application/feetmap.bat
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ..   
-git pull https://github.com/tomaskotian/feetmap.git main
-C:\Users\tomas\AppData\Local\Microsoft\WindowsApps\python.exe D:\\DAYS_IN_LOVE\\feetmap\\code\\feetmap_v0_application\\feetmap_main_v0.py
-
-pause
