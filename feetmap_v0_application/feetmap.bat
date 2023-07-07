86a67a52e637055ea82f8619d5daa80e03be7bf3
tomaskotian <11tomas16@gmail.com>
2023-07-07 16:06:46 +0200

    function prototype

--- feetmap_v0_application/feetmap.bat
+++ feetmap_v0_application/feetmap.bat
@@ -3,4 +3,4 @@ cd ..
 git pull https://github.com/tomaskotian/feetmap.git main
 C:\Users\tomas\AppData\Local\Microsoft\WindowsApps\python.exe D:\\DAYS_IN_LOVE\\feetmap\\code\\feetmap_v0_application\\feetmap_main_v0.py
 
-pause
+::pause
